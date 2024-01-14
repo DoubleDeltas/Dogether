42431462fca607d4703e24aa2306e189ad664924
DoubleDeltas <thesmartist@naver.com>
2024-01-14 16:48:11 +0900

    init

---
+++ src/main/java/com/doubledeltas/dogether/DogetherApplication.java
@@ -0,0 +1,13 @@
+package com.doubledeltas.dogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doubledeltas/dogether/DogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doubledeltas.dogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
